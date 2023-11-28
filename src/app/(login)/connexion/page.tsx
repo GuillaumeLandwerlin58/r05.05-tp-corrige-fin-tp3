a727e1f6efc36a5556d52f73af76aebccdb978dc
GuillaumeLandwerlin58 <guillaume.landwerlin@gmail.com>
2023-11-28 10:09:22 +0100

    question 2.1

--- src/app/(login)/connexion/page.tsx
+++ src/app/(login)/connexion/page.tsx
@@ -1,26 +1,55 @@
 "use client";
 
 import {useForm} from "@mantine/form";
-import {PasswordInput, TextInput} from "@mantine/core";
+import {PasswordInput, TextInput, Box} from "@mantine/core";
+import {Button} from "tp-kit/components";
+import * as z from "zod";
+
+const schema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+});
+
+type FormValues = z.infer<typeof schema>;
+
+type schema = {
+    email: string;
+    password: string;
+}
+
 
 export default function Connexion(){
-  const form = useForm();
+  const form = useForm(
+    {
+      initialValues: {
+        email: "",
+        password: ""
+      },
+    }
+  );
 
   return (
-      <div>
-          <TextInput
-            withAsterisk
-            variant="filled"
-            label="Adresse e-mail"
-            placeholder="Entrez votre adresse e-mail"
-          />
+      <Box maw={340} mx="auto">
+          <form onSubmit={form.onSubmit((values)=>console.log(values))} className="space-y-8 mt-16">
+              <TextInput
+                withAsterisk
+                variant="filled"
+                label="Adresse e-mail"
+                placeholder="Entrez votre adresse e-mail"
+                {...form.getInputProps("email")}
+              />
 
-          <PasswordInput
-            withAsterisk
-            variant="filled"
-            label="Mot de passe"
-            placeholder="Entrez votre mot de passe"
-          />
-      </div>
+              <PasswordInput
+                withAsterisk
+                variant="filled"
+                label="Mot de passe"
+                placeholder="Entrez votre mot de passe"
+                {...form.getInputProps("password")}
+              />
+
+              <Button variant="primary" type="submit">Se connecter</Button>
+              <Button variant="ghost" type="button">Créer un compte</Button>
+          </form>
+      </Box>
   );
 }
--- src/app/(login)/inscription/page.tsx
+++ src/app/(login)/inscription/page.tsx
@@ -2,14 +2,77 @@
 
 import React from 'react';
 import {useForm} from "@mantine/form";
-import {PasswordInput, TextInput} from "@mantine/core";
+import {PasswordInput, TextInput, Box} from "@mantine/core";
+import {Button} from "tp-kit/components";
+import * as z from "zod";
+
+const schema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+    nom: z.string().min(1),
+});
+
+type FormValues = z.infer<typeof schema>;
+
+type schema = {
+    email: string;
+    password: string;
+    nom: string;
+}
+
 
 export default function Inscription(){
-    const form = useForm();
+    const form = useForm(
+        {
+            initialValues: {
+                email: "",
+                password: ""
+            },
+        }
+    );
 
     return (
-        <div>
+        <Box maw={340} mx="auto">
+            <form onSubmit={form.onSubmit((values)=>console.log(values))} className="space-y-8 mt-16">
+                <TextInput
+                    withAsterisk
+                    variant="filled"
+                    label="Nom"
+                    placeholder="Entrez votre nom"
+                    {...form.getInputProps("nom")}
+                />
 
-        </div>
+                <TextInput
+                    withAsterisk
+                    variant="filled"
+                    label="Adresse e-mail"
+                    placeholder="Entrez votre adresse e-mail"
+                    {...form.getInputProps("email")}
+                />
+
+                <PasswordInput
+                    withAsterisk
+                    variant="filled"
+                    label="Mot de passe"
+                    placeholder="Entrez votre mot de passe"
+                    {...form.getInputProps("password")}
+                />
+
+                <Button
+                    variant="primary"
+                    type="submit"
+                >
+                    Se connecter
+                </Button>
+                <Button
+                    variant="ghost"
+                    type="button"
+                >
+                    Déja un compte ? Se connecter
+                </Button>
+
+
+            </form>
+        </Box>
     );
 }
